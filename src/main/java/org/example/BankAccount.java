5f13ddb2c6763fe8c7fc0142f3ff229ddccffbab
Robert <johnrobert.siglos@gcash.com>
2025-07-03 15:20:19 +0800

    Added required methods

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -1,27 +1,55 @@
 package org.example;
 
 public class BankAccount {
-    private int accountNumber;
+    private String accountNumber;
     private String bankAccountHolderName;
     private double availableBalance;
 
 
-    BankAccount(int accountNumber, String bankAccountHolderName, double initialDeposit){
+    BankAccount(String accountNumber, String bankAccountHolderName, double initialDeposit){
         this.accountNumber = accountNumber;
         this.bankAccountHolderName = bankAccountHolderName;
         this.availableBalance = initialDeposit;
     }
 
-    BankAccount(int accountNumber, String bankAccountHolderName){
+    BankAccount(String accountNumber, String bankAccountHolderName){
         this.accountNumber = accountNumber;
         this.bankAccountHolderName = bankAccountHolderName;
     }
 
-    public int getAccountNumber() {
+    public void deposit(double depositAmount){
+        //Validate deposit amount must greater than 0
+        if (depositAmount <= 0) {
+            System.out.println("Deposit Amount must be greater than 0.");
+            return;
+        }
+        else{
+            this.availableBalance += depositAmount;
+        }
+    }
+
+    public void withdraw(double withdrawAmount){
+        //Validate withdraw amount must be less than or equal to available balance
+        if (withdrawAmount > availableBalance){
+            System.out.println("Withdraw Amount is greater than Available Balance.");
+            return;
+        }
+        else{
+            this.availableBalance -= withdrawAmount;
+        }
+    }
+
+    public void displayInformation(){
+        System.out.println("Account Number: " + this.getAccountNumber());
+        System.out.println("Bank Account Holder Name: " + this.getBankAccountHolderName());
+        System.out.println("Available Balance: " + this.getAvailableBalance());
+    }
+    
+    public String getAccountNumber() {
         return accountNumber;
     }
 
-    public void setAccountNumber(int accountNumber) {
+    public void setAccountNumber(String accountNumber) {
         this.accountNumber = accountNumber;
     }
 
